6b2f0a8580952c062e5970359c44abae826e81cc
SevenCatsHere <snowcolton@hotmail.com>
2020-06-07 18:11:48 -0400

    connect to channels

--- packages/psybot/src/__tests__/index.spec.js
+++ packages/psybot/src/__tests__/index.spec.js
@@ -6,8 +6,13 @@ const middleware = require('../middleware');
 
 jest.mock('irc-framework', () => {
 	class MockClient {}
+	MockClient.prototype.on = jest.fn();
 	MockClient.prototype.use = jest.fn();
 	MockClient.prototype.connect = jest.fn();
+	MockClient.prototype.channel = jest.fn().mockReturnValue({
+		join: jest.fn(),
+		updateUsers: jest.fn(),
+	});
 	return { Client: MockClient };
 });
 
@@ -30,6 +35,7 @@ test('Does not apply the debug middleware if process.env.DEBUG is not "true"', (
 		host: 'mockHost',
 		port: 1337,
 		nick: 'RadMan1337',
+		channels: [],
 	}))
 		.toBeInstanceOf(Client);
 	expect(middleware.debug).not.toHaveBeenCalled();
@@ -42,6 +48,20 @@ test('Applies middleware if process.env.DEBUG is "true"', () => {
 		host: 'mockHost',
 		port: 1337,
 		nick: 'RadMan1337',
+		channels: [],
 	});
 	expect(middleware.debug).toHaveBeenCalled();
+});
+
+test('Joins all channels defined in configuration', () => {
+	const client = createPsybot({
+		host: 'mockHost',
+		port: 1337,
+		nick: 'RadMan1337',
+		channels: ['#foo', '#bar', '#baz'],
+	});
+	const registeredHandler = client.on.mock.calls[0][1];
+	registeredHandler();
+	expect(Client.prototype.channel).toHaveBeenCalledTimes(3);
+	expect(Client.prototype.channel.mock.calls).toEqual([['#foo'], ['#bar'], ['#baz']]);
 });
--- packages/psybot/src/index.js
+++ packages/psybot/src/index.js
@@ -9,6 +9,15 @@ module.exports = function createPsybot(config) {
 	if (process.env.DEBUG === 'true') client.use(middleware.debug());
 	client.use(middleware.nickserv(config));
 
+	client.on('registered', () => {
+		config.channels
+			.map(channel => client.channel(channel))
+			.forEach(channel => {
+				channel.join();
+				channel.updateUsers();
+			});
+	});
+
 	client.connect({
 		host: config.host,
 		port: config.port,
